ed2158c2fe98febf60f8801574ce634b0baad750
tiagobmatheus <tiagobmatheus@gmail.com>
2026-09-01 20:57:20 -0300

    Refactor comments and maintain functionality

--- LT01_Ex01.py
+++ LT01_Ex01.py
@@ -1,4 +1,6 @@
 #Algoritmo LT01_EX01
+#Coletar o valor do lado de um quadrado, calcular sua área e apresentar o resultado.
+
 #inicio.
 lado = float(input('Digite o valor do lado do quadrado e tecle Enter: '))
 area = lado * lado
