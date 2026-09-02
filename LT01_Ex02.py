a82523271d6beae3e49a712e989445926d94e7c9
tiagobmatheus <tiagobmatheus@gmail.com>
2026-09-01 21:04:27 -0300

    Add files via upload
    Exercício da Aula de Sistemas Operacionais - Lote 01 - Exercício 02
    Receba o salário de um funcionário e mostre o novo salário com reajuste de 15%.

---
+++ LT01_Ex02.py
@@ -0,0 +1,7 @@
+#Algoritmo LT01_EX02
+#Receba o salário de um funcionário e mostre o novo salário com reajuste de 15%.
+#inicio.
+Salario = float(input('Digite o valor do Salário do Funcionário e tecle Enter: '))
+Salario_Reajustado = Salario * 1.15
+print('O novo salário com reajuste de 15% é:', Salario_Reajustado)
+#fim.
